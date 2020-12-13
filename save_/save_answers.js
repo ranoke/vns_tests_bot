66d5911029d0a3b1c6546369a190912989df42f8
RANOK <43960920+ranoke@users.noreply.github.com>
2020-12-13 08:05:51 -0800

    added support for more tasks
    with_pre_defined_numbers
    with_pre_defined_letters

--- save_/save_answers.js
+++ save_/save_answers.js
@@ -1,10 +1,4 @@
-
-function Answer(question, answer) {
-  this.question = question;
-  this.answer = answer;
-}
-
-const downloadToFile = (content, filename, contentType) => {
+const download_to_file = (content, filename, contentType) => {
   const a = document.createElement('a');
   const file = new Blob([content], {type: contentType});
   
@@ -15,39 +9,65 @@ const downloadToFile = (content, filename, contentType) => {
 	URL.revokeObjectURL(a.href);
 };
 
+function answer_t(question, answer, type) 
+{
+  this.question = question;
+  this.answer = answer;
+  this.type = type;
+}
 
-console.log("Save");
+function get_answer_type(obj)
+{
+    if(obj.getElementsByClassName("answernumber")) return "with_define_answer_number";
+    else return "true_false"
+    return "unknown";
+}
 
+function get_answer_text(obj, type)
+{
+    if(type === "true_false")
+    {
+        return obj.textContent;
+    }
+    else if(type === "with_define_answer_number")
+    {
+        var l = 0;
+        if(obj.getElementsByClassName("answernumber").length > 0)
+        {
+            let text = obj.getElementsByClassName("answernumber")[0].textContent;
+            l = text.length;
+
+        }
+        return obj.innerText.substring(l);
+    }
+}
+
+
+
+// for html recognition
 var answer_icon = "icon fa fa-check text-success fa-fw "
 var qtext = "qtext";
 
-var aJson = new Object();
-aJson.answer = [];
+var answerJson = new Object();
+answerJson.answer = [];
 
-
-
-let answers = document.getElementsByClassName(answer_icon);
-let questions = document.getElementsByClassName(qtext);
-for(var i = 0; i < answers.length; i++)
+let questionObjects = document.getElementsByClassName(qtext);
+let answerObjects = document.getElementsByClassName(answer_icon);
+for(var i = 0; i < questionObjects.length; i++)
 {
-    //console.log(answers[i].textContent);
-    var ans = answers[i].parentElement.getElementsByTagName("label")[0].textContent;
-    var q = questions[i].getElementsByTagName("p")[0].textContent;
-    aJson.answer.push(new Answer(q, ans));
-
+    let questionObject = questionObjects[i].getElementsByTagName("p")[0];
+    let answerObject = answerObjects[i].parentElement.getElementsByTagName("label")[0];
+    let type = get_answer_type(answerObject);
+    let answer_text = get_answer_text(answerObject, type);
+    console.log(questionObject.textContent, answer_text);
+    answerJson.answer.push(new answer_t(questionObject.textContent, answer_text, type));
 }
 
 let nav_bar = document.getElementById("page-navbar");
 let names = nav_bar.getElementsByTagName("li");
+answerJson.name = names[names.length - 1].getElementsByTagName("a")[0].textContent;
+answerJson.count = answerObjects.length;
 
-aJson.name = names[names.length - 1].getElementsByTagName("a")[0].textContent;
+var jsonString = JSON.stringify(answerJson);
+download_to_file(jsonString, answerJson.name + ".json", "text/plain");
 
-aJson.count = answers.length;
-
-var jsonString = JSON.stringify(aJson);
-
-
-
-console.log(jsonString);
-
-downloadToFile(jsonString, aJson.name + ".json", "text/plain");
